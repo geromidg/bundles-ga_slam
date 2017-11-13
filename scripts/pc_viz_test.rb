f5a2b7422689ada2202e561a7747bf293a3f92db
Dimitris Geromichalos <geromidg@gmail.com>
2017-11-13 09:39:24 +0100

    Remove cartographer from viz script

--- scripts/pc_viz_test.rb
+++ scripts/pc_viz_test.rb
@@ -14,8 +14,7 @@ Bundles.transformer.load_conf(
 Orocos.run "camera_bb2::Task" => "camera_bb2",
            "stereo::Task" => "stereo",
            "viso2::StereoOdometer" => "viso2",
-           "cartographer::Task" => "cartographer",
-           # :gdb => ['cartographer'],
+           # :gdb => ['stereo'],
            # :valgrind => ['stereo'],
            :valgrind_options => ['--track-origins=yes'],
            :output => '%m-%p.log' \
@@ -38,23 +37,15 @@ do
     Bundles.transformer.setup(viso2)
     viso2.configure
 
-    cartographer = TaskContext.get 'cartographer'
-    Orocos.conf.apply(cartographer, ['default'], :override => true)
-    Bundles.transformer.setup(cartographer)
-    cartographer.configure
-
     bag.camera_firewire_bb2.frame.connect_to    camera_bb2.frame_in
     camera_bb2.left_frame.connect_to            stereo.left_frame
     camera_bb2.right_frame.connect_to           stereo.right_frame
     camera_bb2.left_frame.connect_to            viso2.left_frame
     camera_bb2.right_frame.connect_to           viso2.right_frame
-    stereo.distance_frame.connect_to            cartographer.distance_image
-    viso2.pose_samples_out.connect_to           cartographer.pose_in
 
     camera_bb2.start
     stereo.start
     viso2.start
-    sleep 1; cartographer.start
 
     ####### Vizkit #######
 
@@ -63,15 +54,14 @@ do
     control_1.seek_to 1000
     control_1.bplay_clicked
 
-    control_2 = Vizkit.control gps
-    control_2.speed = 1
-    control_2.seek_to 3562
-    control_2.bplay_clicked
+    # control_2 = Vizkit.control gps
+    # control_2.speed = 1
+    # control_2.seek_to 3562
+    # control_2.bplay_clicked
 
     Vizkit.display camera_bb2.left_frame
     Vizkit.display stereo.point_cloud
-    # Vizkit.display viso2.point_cloud_samples_out
-    # Vizkit.display cartographer.traversability_map
+    Vizkit.display viso2.point_cloud_samples_out
 
     # Vizkit.display gps.gps_heading.pose_samples_out,
         # :widget => Vizkit.default_loader.RigidBodyStateVisualization
