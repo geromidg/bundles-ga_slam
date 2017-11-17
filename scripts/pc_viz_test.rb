4bbdf31e867d13704a1acf55e51d0c11298da979
Dimitris Geromichalos <geromidg@gmail.com>
2017-11-17 18:21:39 +0100

    Add pancam and cleanup pc_viz_test script

--- scripts/pc_viz_test.rb
+++ scripts/pc_viz_test.rb
@@ -9,26 +9,34 @@ Bundles.initialize
 Bundles.transformer.load_conf(
     Bundles.find_file('config', 'transforms_scripts.rb'))
 
-Orocos.run "camera_bb2::Task" => "camera_bb2",
-           "stereo::Task" => "stereo",
-           "viso2::StereoOdometer" => "viso2",
-           "cartographer::Task" => "cartographer",
-           # :output => '%m-%p.log' \
-           # :gdb => ['cartographer'],
-           # :valgrind => ['stereo'],
-           :valgrind_options => ['--track-origins=yes'] \
+Orocos.run(
+    ####### Tasks #######
+    'camera_bb2::Task' => 'camera_bb2',
+    'stereo::Task' => 'stereo',
+    'viso2::StereoOdometer' => 'viso2',
+    'cartographer::Task' => 'cartographer',
+    ####### Debug #######
+    # :output => '%m-%p.log',
+    # :gdb => ['cartographer'],
+    # :valgrind => ['stereo'],
+    :valgrind_options => ['--track-origins=yes']) \
 do
+    ####### Replay #######
     bag = Orocos::Log::Replay.open(
-        "~/rock_bags/bb2.log",
-        "~/rock_bags/gps.log")
+        '~/rock_bags/bb2.log',
+        '~/rock_bags/pancam.log',
+        '~/rock_bags/waypoint_navigation.log',
+        # '~/rock_bags/imu.log',
+    )
     bag.use_sample_time = true
 
+    ####### Configure #######
     camera_bb2 = TaskContext.get 'camera_bb2'
     Orocos.conf.apply(camera_bb2, ['default'], :override => true)
     camera_bb2.configure
 
     stereo = TaskContext.get 'stereo'
-    Orocos.conf.apply(stereo, ['hdpr_bb2'], :override => true)
+    Orocos.conf.apply(stereo, ['panCam'], :override => true)
     stereo.configure
 
     viso2 = TaskContext.get 'viso2'
@@ -40,24 +48,27 @@ do
     Bundles.transformer.setup(cartographer)
     cartographer.configure
 
-    bag.camera_firewire_bb2.frame.connect_to    camera_bb2.frame_in
-    camera_bb2.left_frame.connect_to            stereo.left_frame
-    camera_bb2.right_frame.connect_to           stereo.right_frame
-    camera_bb2.left_frame.connect_to            viso2.left_frame
-    camera_bb2.right_frame.connect_to           viso2.right_frame
-    stereo.distance_frame.connect_to            cartographer.distance_image
-    viso2.pose_samples_out.connect_to           cartographer.pose_imu
+    ####### Connect #######
+    bag.camera_firewire_bb2.frame.connect_to        camera_bb2.frame_in
+    bag.pancam_panorama.left_frame_out.connect_to   stereo.left_frame
+    bag.pancam_panorama.right_frame_out.connect_to  stereo.right_frame
+    camera_bb2.left_frame.connect_to                viso2.left_frame
+    camera_bb2.right_frame.connect_to               viso2.right_frame
 
+    stereo.distance_frame.connect_to                cartographer.distance_image
+    viso2.pose_samples_out.connect_to               cartographer.pose_imu
+    # bag.gps_heading.pose_samples_out.connect_to     cartographer.pose_imu
+
+    ####### Start #######
     camera_bb2.start
     stereo.start
     viso2.start
     cartographer.start
 
     ####### Vizkit #######
-
     control = Vizkit.control bag
     control.speed = 1
-    control.seek_to 1000
+    control.seek_to 1500
     control.bplay_clicked
 
     Vizkit.display viso2.pose_samples_out,
@@ -65,15 +76,17 @@ do
     Vizkit.display viso2.pose_samples_out,
         :widget => Vizkit.default_loader.TrajectoryVisualization
     # Vizkit.display bag.gps_heading.pose_samples_out,
-        # :widget => Vizkit.default_loader.RigidBodyStateVisualization
+        # :widget => Vizkit.default_loader.TrajectoryVisualization
 
-    # Vizkit.display camera_bb2.left_frame
+    Vizkit.display camera_bb2.left_frame
+    Vizkit.display bag.pancam_panorama.left_frame_out
+
     # Vizkit.display stereo.point_cloud
     # Vizkit.display viso2.point_cloud_samples_out
     # Vizkit.display cartographer.heightMap_frame
     # Vizkit.display cartographer.heightMapInterp_frame
     # Vizkit.display cartographer.pointcloud_in
-    Vizkit.display cartographer.pointcloud_filter
+    # Vizkit.display cartographer.pointcloud_filter
 
     Vizkit.exec
 end
