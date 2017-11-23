5cb838a712582a9d57fbad854a07a8abd75831a1
Dimitris Geromichalos <geromidg@gmail.com>
2017-11-23 16:14:38 +0100

    Cleanup transforms script

--- config/transforms_scripts.rb
+++ config/transforms_scripts.rb
@@ -1,14 +1,60 @@
+Quaternion = Eigen::Quaternion
+Vector = Eigen::Vector3
+PI = Math::PI
+
 static_transform \
-    Eigen::Quaternion.from_euler(
-        Eigen::Vector3.new(-1.5666, 0.02348, -2.145), 2, 1, 0),
-    Eigen::Vector3.new(0.527, 0.0738, 0.0588),
-    "left_camera_viso2" => "body"
+    Quaternion.from_euler(Vector.new(
+        -PI/2,
+        0.023,
+        -PI/2 - 0.574),
+        2, 1, 0),
+    Vector.new(
+        0.527,
+        0.073,
+        0.858),
+    "left_camera_bb2" => "body"
 
-static_transform Eigen::Quaternion.from_angle_axis(
-    Math::PI, Eigen::Vector3.UnitZ),
-    Eigen::Vector3.new( 0.0, 0.0, 0.0 ),
-    "imu" => "body"
+static_transform \
+    Quaternion.from_euler(Vector.new(
+        -PI/2 + 0.005,
+        0.321,
+        -PI/2 - 0.066),
+        2, 1, 0),
+    Vector.new(
+        0.426,
+        0.129,
+        1.491),
+    "left_camera_bb3" => "body"
 
-dynamic_transform "viso2.pose_samples_out", "body" => "world_osg"
-dynamic_transform "viso2.pose_samples_out", "body" => "viso_world"
+static_transform \
+    Quaternion.from_euler(Vector.new(
+        -PI/2,
+        0.0,
+        -PI/2),
+        2, 1, 0),
+    Vector.new(
+        0.010,
+        0.250,
+        0.055),
+    "left_camera_pancam" => "ptu"
+
+static_transform \
+    Quaternion.from_euler(Vector.new(
+        -PI/2,
+        -0.050 + 0.392,
+        -PI/2),
+        2, 1, 0),
+    Vector.new(
+        0.138,
+        -0.005,
+        1.860),
+    "ptu" => "body"
+
+static_transform Quaternion.Identity(), Vector.Zero(),
+    "left_camera_viso2" => "left_camera_bb2"
+
+static_transform Quaternion.Identity(), Vector.Zero(),
+    # "slamCamera" => "left_camera_bb2"
+    "slamCamera" => "left_camera_bb3"
+    # "slamCamera" => "left_camera_pancam"
 
