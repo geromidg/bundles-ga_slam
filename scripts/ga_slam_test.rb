9896dee6a04c14c0a9639e9687b1fcdd44d0820a
Dimitris Geromichalos <geromidg@gmail.com>
2017-11-27 13:52:03 +0100

    Disable bb2 and viso2 from GA SLAM script

--- scripts/ga_slam_test.rb
+++ scripts/ga_slam_test.rb
@@ -11,10 +11,10 @@ Bundles.transformer.load_conf(
 
 Orocos.run(
     ####### Tasks #######
-    'camera_bb2::Task' => 'camera_bb2',
+    # 'camera_bb2::Task' => 'camera_bb2',
     'camera_bb3::Task' => 'camera_bb3',
     'stereo::Task' => 'stereo',
-    'viso2::StereoOdometer' => 'viso2',
+    # 'viso2::StereoOdometer' => 'viso2',
     'ga_slam::Task' => 'ga_slam',
     ####### Debug #######
     # :output => '%m-%p.log',
@@ -24,17 +24,17 @@ Orocos.run(
 do
     ####### Replay #######
     bag = Orocos::Log::Replay.open(
-        '~/rock_bags/bb2.log',
+        # '~/rock_bags/bb2.log',
         '~/rock_bags/bb3.log',
-        '~/rock_bags/waypoint_navigation.log',
+        # '~/rock_bags/waypoint_navigation.log',
         '~/rock_bags/imu.log',
     )
     bag.use_sample_time = true
 
     ####### Configure #######
-    camera_bb2 = TaskContext.get 'camera_bb2'
-    Orocos.conf.apply(camera_bb2, ['default'], :override => true)
-    camera_bb2.configure
+    # camera_bb2 = TaskContext.get 'camera_bb2'
+    # Orocos.conf.apply(camera_bb2, ['default'], :override => true)
+    # camera_bb2.configure
 
     camera_bb3 = TaskContext.get 'camera_bb3'
     Orocos.conf.apply(camera_bb3, ['default'], :override => true)
@@ -44,27 +44,27 @@ do
     Orocos.conf.apply(stereo, ['hdpr_bb3_left_right'], :override => true)
     stereo.configure
 
-    viso2 = TaskContext.get 'viso2'
-    Orocos.conf.apply(viso2, ['bumblebee'], :override => true)
-    Bundles.transformer.setup(viso2)
-    viso2.configure
+    # viso2 = TaskContext.get 'viso2'
+    # Orocos.conf.apply(viso2, ['bumblebee'], :override => true)
+    # Bundles.transformer.setup(viso2)
+    # viso2.configure
 
     ga_slam = TaskContext.get 'ga_slam'
     Bundles.transformer.setup(ga_slam)
     ga_slam.configure
 
     ####### Connect #######
-    bag.camera_firewire_bb2.frame.connect_to                camera_bb2.frame_in
-    bag.camera_firewire_bb3.frame.connect_to                camera_bb3.frame_in
-    camera_bb3.left_frame.connect_to                        stereo.left_frame
-    camera_bb3.right_frame.connect_to                       stereo.right_frame
-    camera_bb2.left_frame.connect_to                        viso2.left_frame
-    camera_bb2.right_frame.connect_to                       viso2.right_frame
+    # bag.camera_firewire_bb2.frame.connect_to              camera_bb2.frame_in
+    bag.camera_firewire_bb3.frame.connect_to              camera_bb3.frame_in
+    camera_bb3.left_frame.connect_to                      stereo.left_frame
+    camera_bb3.right_frame.connect_to                     stereo.right_frame
+    # camera_bb2.left_frame.connect_to                      viso2.left_frame
+    # camera_bb2.right_frame.connect_to                     viso2.right_frame
 
-    stereo.point_cloud.connect_to                           ga_slam.pointCloud
-    # viso2.pose_samples_out.connect_to                   ga_slam.pose
-    # bag.gps_heading.pose_samples_out.connect_to         ga_slam.pose
-    bag.imu_stim300.orientation_samples_out.connect_to      ga_slam.pose
+    stereo.point_cloud.connect_to                         ga_slam.pointCloud
+    # viso2.pose_samples_out.connect_to                     ga_slam.pose
+    # bag.gps_heading.pose_samples_out.connect_to           ga_slam.pose
+    bag.imu_stim300.orientation_samples_out.connect_to    ga_slam.pose
 
     ####### Start #######
     # camera_bb2.start
@@ -76,7 +76,8 @@ do
     ####### Vizkit #######
     control = Vizkit.control bag
     control.speed = 1
-    control.seek_to 2600
+    # control.seek_to 60000
+    control.seek_to 15500
     control.bplay_clicked
 
     # Vizkit.display viso2.pose_samples_out,
